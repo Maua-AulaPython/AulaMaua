c93ca01bf2e840a1ae720a1dac023ccb5001a426
BruMenasce <bru180@gmail.com>
2015-03-20 11:11:28 -0300

    Create EX3_médias2.py

---
+++ EX3_médias2.py
@@ -0,0 +1,20 @@
+import math
+
+
+lista = []
+
+def media(lista):
+    i = 0
+    soma = 0
+    for i in range (len(lista)):
+        soma = soma + lista[i]
+        media = soma/(len(lista))
+    return media
+    
+a = 0
+while a>=0:
+    a = int(raw_input('digite a nota  '))
+    lista.append(a)
+    
+    
+print media (lista[:-1])
