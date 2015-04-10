a3474b14bdb756c38303d7b4b52c35c853d54b7c
BruMenasce <bru180@gmail.com>
2015-04-10 10:57:37 -0300

    Create Ex8_Medias.py

---
+++ Ex8_Medias.py
@@ -0,0 +1,28 @@
+import math
+import sys
+
+lista = []
+
+def media(lista):
+    i = 0
+    soma = 0
+    for i in range (len(lista)):
+        soma = soma + lista[i]
+        media = soma/(len(lista))
+    return media
+
+maior = 0   
+a = 0
+while a>=0:
+    while True:
+        try:
+            a = float(raw_input('digite a nota  '))
+            if (maior < a):
+                maior = a
+            break
+        except:
+            print ' voce nao digitou um numero valido'
+    lista.append(a)
+   
+print media (lista[:-1])
+print ' a maior note é: {0}' .format(maior)
